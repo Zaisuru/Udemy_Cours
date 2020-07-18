6574d0efde01d3f2cdcb089c69c6029b127f43b1
Holderbaum <holderbaum.thomas@hotmail.fr>
2020-07-18 17:28:53 +0200

    Array

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,21 @@
-alert("Hello");
+var nom ="Thomas";
+var age = 24;
+
+var texte  = "Bonjour " + nom + " Tu as " + age + " ans"; 
+
+console.log(texte);
+
+
+var prenom = "Thomas";
+console.log(prenom[2]); // Récupération de la lettre placer en 2 
+console.log(prenom[prenom.length -1]); // Récupération de la dernière lettre 
+
+// Arrays
+var stats =["attaque", "défense"]
+var array =[nom, age, prenom, stats];
+
+array[0] ="Holder";
+array[1] ="30";
+array[3][1] ="Puissance";
+
+console.log(array);
