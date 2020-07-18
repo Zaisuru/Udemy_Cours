9ec3a625b4eeaf61759b65b76ca6ae3059fc567c
Holderbaum <holderbaum.thomas@hotmail.fr>
2020-07-18 17:40:59 +0200

    Array TP Correct

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,4 +1,4 @@
-var nom ="Thomas";
+var nom ="Holderbaum";
 var age = 24;
 
 var texte  = "Bonjour " + nom + " Tu as " + age + " ans"; 
@@ -17,5 +17,20 @@ var array =[nom, age, prenom, stats];
 array[0] ="Holder";
 array[1] ="30";
 array[3][1] ="Puissance";
-
 console.log(array);
+
+var arr = [1, 2, 3];
+arr.push("Salut"); // Rajouter un élément dans la liste
+arr.pop(); // Suppr la dernière entrée
+arr.unshift(0); // Rajoute au début
+arr.shift()//Suppr la première entrée 
+
+console.log(arr);
+
+var initial =prenom[0] + nom[0];
+var result = [nom, prenom, initial];
+console.log(result)
+
+var correct = [];
+correct.push(prenom, nom, initial);
+console.log(correct)
