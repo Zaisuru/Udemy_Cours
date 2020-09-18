a5b8c10c86c76c2a10e0ddee407b24fd489af5a4
Zaisuru <thomas.holderbaum@gmail.com>
2020-09-18 14:36:46 +0200

    Add old learn

---
+++ Travel_Agency/Travel_Agency/index2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel='stylesheet' href='Style.css'>    
+        <link href='http://fonts.googleapis.com/css?family=Crete+Round' rel='stylesheet'>
+        <title>Travel Agency</title>
+    </head>
+
+    <body>
+        <header>
+                 <a href="index.html" class='button4'>X</a>
+        </header>
+
+        <div id='youtube'>
+        <iframe width="500" height="315"
+            src="https://www.youtube.com/watch?v=yQ3zPr5nFWw" frameborder="0" >    
+        </iframe>
+    </div>
+    </body>
+
+
+
+
+</html>
